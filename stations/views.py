19ca5fa795d2ef27e72dc46023f65cc77d8944e1
Yuriy Chekmaev <y.chekmaev@mail.ru>
2022-04-11 16:31:19 +0300

    Первый коммит

---
+++ stations/views.py
@@ -0,0 +1,31 @@
+import csv
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+CONTENT = []
+
+
+with open('data-398-2018-08-30.csv', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for news in reader:
+        CONTENT.append(news)
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+def bus_stations(request):
+    # получите текущую страницу и передайте ее в контекст
+    # также передайте в контекст список станций на странице
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(CONTENT, 10)
+    page = paginator.get_page(page_number)
+    context = {
+        'bus_stations': page,
+        'page': page
+    }
+    return render(request, 'stations/index.html', context)
